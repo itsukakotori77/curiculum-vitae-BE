27c013d6518e439769425c5f68d1a5babc5c234f
ikotori55 <ikotori55@gmail.com>
2025-05-14 14:42:34 +0700

    feat: controller experience

--- src/modules/curiculum-experience/curiculum-experience.controller.ts
+++ src/modules/curiculum-experience/curiculum-experience.controller.ts
@@ -1,4 +1,124 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common'
+import { CuriculumExperienceService } from './curiculum-experience.service'
+import { PaginationPayloadDto } from 'src/global/dto/pagination-payload-dto'
+import { Response } from 'express'
+import { CuriculumExperienceDto } from './curiculum-experience-dto'
 
 @Controller('curiculum-experience')
-export class CuriculumExperienceController {}
+export class CuriculumExperienceController {
+  constructor(private curExpService: CuriculumExperienceService) {}
+
+  async getAll(@Body() request: PaginationPayloadDto, @Res() res: Response) {
+    try {
+      const data = await this.curExpService.getAll(request)
+      return res.status(HttpStatus.OK).json({
+        code: '00',
+        message: 'Berhasil menampilkan data',
+        data: data.data,
+        total_data: data.totalData,
+        total_page: data.totalPage,
+        current_page: data.currentPage,
+      })
+    } catch (error: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        code: '01',
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+
+  @Get('getOne/:id')
+  async getById(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const data = await this.curExpService.getById(id)
+      if (data) {
+        return res.status(HttpStatus.OK).json({
+          code: '00',
+          message: 'Berhasil menampilkan data',
+          data: data,
+        })
+      }
+
+      return res.status(HttpStatus.NOT_FOUND).json({
+        code: '01',
+        message: 'Data tidak ditemukan',
+      })
+    } catch (error: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        code: '01',
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+
+  @Post('create')
+  async create(@Body() request: CuriculumExperienceDto, @Res() res: Response) {
+    try {
+      const data = await this.curExpService.create(request)
+      return res.status(HttpStatus.OK).json({
+        code: '00',
+        message: 'Berhasil menambahkan data',
+        data: data,
+      })
+    } catch (error: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        code: '01',
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+
+  @Put('update')
+  async update(@Body() request: CuriculumExperienceDto, @Res() res: Response) {
+    try {
+      const data = await this.curExpService.update(request)
+      return res.status(HttpStatus.OK).json({
+        code: '00',
+        message: 'Berhasil mengubah data',
+        data: data,
+      })
+    } catch (error: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        code: '01',
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+
+  async delete(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const data = await this.curExpService.delete(id)
+
+      if (data) {
+        return res.status(HttpStatus.OK).json({
+          code: '00',
+          message: 'Berhasil menghapus data',
+        })
+      }
+
+      return res.status(HttpStatus.NOT_FOUND).json({
+        code: '01',
+        message: 'Data tidak ditemukan',
+      })
+    } catch (error: any) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        code: '01',
+        success: false,
+        message: error.message,
+      })
+    }
+  }
+}
