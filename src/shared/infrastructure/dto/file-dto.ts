e5208ad356f5ef9a3661beef0c8d9fbe85918178
itsukakotori55 <ikotori55@gmail.com>
2025-11-20 20:39:51 +0700

    chore: file dto

--- src/shared/infrastructure/dto/file-dto.ts
+++ src/shared/infrastructure/dto/file-dto.ts
@@ -28,3 +28,29 @@ export class FileBodyDto {
   @IsString()
   description?: string
 }
+export class FileUploadDto {
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    description: 'Image file',
+  })
+  file: Express.Multer.File
+
+  @ApiProperty({
+    description: 'Folder name',
+    example: 'uploads',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  folder?: string
+
+  @ApiProperty({
+    description: 'File description',
+    example: 'Profile image',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  description?: string
+}
